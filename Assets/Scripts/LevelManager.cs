c606855a963de4a5af3e9b50da7c6b2ed891da46
Wira Megantara Pamungkas <89589061+pamungkaswira27@users.noreply.github.com>
2023-10-29 17:16:05 +0700

    feat: add expand room

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,10 +4,14 @@ public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
 
+    [Header("Public Storage Variable")]
     [SerializeField]
     private RoomUpgradeController _roomUpgradeController;
-
     public RoomUpgradeController RoomUpgradeController => _roomUpgradeController;
+
+    [SerializeField]
+    private RoomExpanderController _roomExpanderController;
+    public RoomExpanderController RoomExpanderController => _roomExpanderController;
 
     private void Awake()
     {
